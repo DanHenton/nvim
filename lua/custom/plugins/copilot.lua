4122d4678b3144c4a0349bfc265d6a58965671ea
DJ Henton <danjhenton@gmail.com>
2025-07-25 14:27:38 +1200

    add exlixir-tools

--- lua/custom/plugins/copilot.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'github/copilot.vim',
-  build = ':Copilot setup', -- <--- MAKE SURE THIS LINE IS PRESENT AND UNCOMMENTED
-  event = 'VeryLazy',       -- Or 'InsertEnter', but VeryLazy is usually better
-  opts = {}
-}
